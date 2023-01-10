1c4dca13a3a58b854be67ff0af2fb5c6b34f8071
pierre <pierre.sikora@gmail.com>
2023-01-10 18:50:32 +0100

    UML + Creation GUIParser

--- src/main/java/fr/isep/game7WonderArch/domain/wonder/Wonders.java
+++ src/main/java/fr/isep/game7WonderArch/domain/wonder/Wonders.java
@@ -3,23 +3,23 @@ package fr.isep.game7WonderArch.domain.wonder;
 public enum Wonders {
 
 	Alexandrie("Alexandrie", "Alexandrie", //
-			"Prenez la première carte d'une pioche au choix, n'importe où sur la table, et posez-la devant vous"),
+			"Prenez la premiÃ¨re carte d'une pioche au choix, n'importe oÃ¹ sur la table, et posez-la devant vous"),
 	
 	Halicarnasse("Halicarnasse", "Halicarnasse", //
-			"Prenez les 5 premières cartes de la pioche à votre gauche ou à votre droite, choisissez-en 1 et posez-la devant vous" //
-			+ "Mélangez-les cartes restantes dans leur pioche"),
+			"Prenez les 5 premiÃ¨res cartes de la pioche Ã  votre gauche ou Ã  votre droite, choisissez-en 1 et posez-la devant vous" //
+			+ "MÃ©langez-les cartes restantes dans leur pioche"),
 	
-	Ephese("Ephese", "Ephèse", //
-			"Prenez la première carte de la pioche centrale et posez-la devant vous"),
+	Ephese("Ephese", "EphÃ¨se", //
+			"Prenez la premiÃ¨re carte de la pioche centrale et posez-la devant vous"),
 	
 	Olympie("Olympie", "Olympie", //
-			"Prenez la première carte de la pioche à votre cauche et de celle à votre droite, et posez-les devant vous"),
+			"Prenez la premiÃ¨re carte de la pioche Ã  votre cauche et de celle Ã  votre droite, et posez-les devant vous"),
 	
 	Babylone("Babylone", "Babylone", //
-			"Choisissez 1 jeton Progrès parmi les 4 disponibles, et posez-le devant vous"),
+			"Choisissez 1 jeton ProgrÃ¨s parmi les 4 disponibles, et posez-le devant vous"),
 	
 	Rhodes("Rhodes", "Rhodes", //
-			"Ajoutez 1 Bouclier à votre total de Boucliers"),
+			"Ajoutez 1 Bouclier Ã  votre total de Boucliers"),
 	
 	Gizeh("Gizeh", "Gizeh", //
 			"Cette merveille n'a pas d'effet particulier, mais rapporte plus de points de victoire que les autres Merveilles");
