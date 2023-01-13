454bd625bce5307797d7275ba78b8830abf42dd3
pierre <pierre.sikora@gmail.com>
2023-01-13 09:21:43 +0100

    UML modification

--- src/main/java/fr/isep/game7WonderArch/domain/wonder/Wonder.java
+++ src/main/java/fr/isep/game7WonderArch/domain/wonder/Wonder.java
@@ -1,4 +1,13 @@
 package fr.isep.game7WonderArch.domain.wonder;
 
+import java.util.List;
+import java.util.Map;
+
 public abstract class Wonder {
+    int layersAmount;    List<Map<Integer, Boolean>> parts;
+    Wonder(int layersAmount){
+        this.layersAmount = layersAmount;        SetLayers();
+    }
+    protected abstract void SetLayers();
 }
+
