ec9f3effb14329da40013a6a2f0ff7d1e13a76f6
pierre <pierre.sikora@gmail.com>
2023-01-17 08:53:58 +0100

    playerNumber

--- src/main/java/com/example/game7wondersarchitect/HelloApplication.java
+++ src/main/java/com/example/game7wondersarchitect/HelloApplication.java
@@ -10,10 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Play_now-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        //stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
